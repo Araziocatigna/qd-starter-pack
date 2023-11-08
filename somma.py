01eae2378037f07bf3d733c9fea48c6ad8b547df
GG <gabrielegiannetto7@gmail.com>
2023-11-08 18:27:55 +0100

    aggiornamento file somma.py

--- somma.py
+++ somma.py
@@ -1,4 +1,6 @@
 num1 = int(input("Inserisci il primo numero:  "))
 num2 = int(input("Inserisci il secondo numero:  "))
 def somma (num1: int, num2: int) -> int:
-print (f'La somma di {num1} e {num2} è: {somma}')
+    return num1 + num2
+
+print (f'La somma di {num1} e {num2} è: {somma(num1, num2)}')
