a1130cb450bb8af9785134ce69dd4543f9319337
GG <gabrielegiannetto7@gmail.com>
2023-11-08 18:20:08 +0100

    test somma

--- somma.py
+++ somma.py
@@ -1,4 +1,4 @@
 num1 = int(input("Inserisci il primo numero:  "))
 num2 = int(input("Inserisci il secondo numero:  "))
-somma = num1+num2
+def somma (num1: int, num2: int) -> int:
 print (f'La somma di {num1} e {num2} è: {somma}')
